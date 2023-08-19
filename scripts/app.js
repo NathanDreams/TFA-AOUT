39214f9a7acdec3e378a3b501a7e79a6ee5f435d
Nathan <nathan.collart.1604@gmail.com>
2023-08-19 11:16:56 +0200

    dernier

--- scripts/app.js
+++ scripts/app.js
@@ -84,12 +84,8 @@ window.onload = function () {
     }
 };
 
-// accordéon //
 
-const accordion = document.getElementsByClassName('contentBx');
 
-for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-    });
-}
+
+
+
